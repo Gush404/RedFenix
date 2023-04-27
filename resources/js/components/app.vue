328fdc4eff04c56c4d206e9e47cfa7ef236b62fd
Gush <kakirmsplatoon2@gmail.com>
2023-04-27 10:53:23 +0100

    Tobey helped setup admin panels

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="min-h-screen w-screen bg-gray-800">
       <div class="flex border border-2 h-16 gap-4 items-center space-x-5 justify-between text-red-600 px-4">
-          <div class="flex h-full items-center space-x-2 justify-start gap-4">
-              <router-link :to="{name:'home'}">home</router-link>
-              <router-link :to="{name:'about'}">about</router-link>
-          </div>
-          <router-link to="" tag="button">logout</router-link>
+            <div class="flex h-full items-center space-x-2 justify-start gap-4">
+                <router-link :to="{name:'home'}">home</router-link>
+                <router-link :to="{name:'about'}">about</router-link>
+            </div>
+            <form action="/logout" method="post">
+                <input type="hidden" name="_token" :value="csrf">
+                <button type="submit">Logout</button>
+            </form>
       </div>
       <router-view></router-view>
   </div>
 </template>
 
 <script>
+export default{
+    data: ()=> ({
+        csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+    })
+}
 </script>
 
 <style>
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -32,4 +32,4 @@ require __DIR__.'/auth.php';
 
 Route::get('{any}', function () {
     return view('dashboard');
-})->where('any', '.*')->middleware(['auth', 'verified'])->name('any');
+})->where('any', '.*')->middleware(['auth'])->name('any');
