d0658060bc10782918a6f517d211db33092c1758
Gush <kakirmsplatoon2@gmail.com>
2023-05-05 13:04:00 +0100

    make a table page sucess, pass it to the panel

---
+++ resources/js/pages/table.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th class="text-white">ID</th>
+          <th class="text-white">Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td class="text-white">{{ item.id }}</td>
+          <td class="text-white">{{ item.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    axios.get('/api/items')
+      .then(response => {
+        this.items = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  },
+};
+</script>
